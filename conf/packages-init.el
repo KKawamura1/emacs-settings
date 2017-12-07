9c674d242d4760c7ae23e467949a8342b443a36c
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2017-12-07 18:56:48 +0900

    helmにC-rが取られているためか使えなかったので削除

--- conf/packages-init.el
+++ conf/packages-init.el
@@ -211,12 +211,6 @@ org-modeなどで活用。"
   (bind-key "C-e" 'seq-end)
   )
 
-;;; minibuf-isearch
-(use-package minibuf-isearch
-  :init
-  (setq minibuf-isearch-message-on-right t)
-  )
-
 ;;; ====== 特定のモードで使用するもの ======
 ;;; magit
 (use-package magit
